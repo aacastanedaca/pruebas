58513683cd8e13e63a0106dd05437b83e5d30920
root <metalirio@gmail.com>
2021-06-18 18:18:35 -0500

    subiendo repositorio

---
+++ src/main/java/com/seekmutant/SeekmutantApplication.java
@@ -0,0 +1,13 @@
+package com.seekmutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekmutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekmutantApplication.class, args);
+	}
+
+}
